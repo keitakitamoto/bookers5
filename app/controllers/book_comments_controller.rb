d38707e2a02e085fc87e3c3a125a1719105d14f7
EC2 Default User <ec2-user@ip-172-31-91-42.ec2.internal>
2021-09-07 07:06:03 +0000

    bookers3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -9,6 +9,11 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
+    book = Book.find(params[:book_id])
+    comment = BookComment.find_by(id: params[:id],book_id: book.id)
+    # find by 複数の条件を指定できて、一致した一件の情報を取得する
+    comment.destroy
+    redirect_to book_path(book)
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  attachment :profile_image, destroy: false
+  attachment :profile_image
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
